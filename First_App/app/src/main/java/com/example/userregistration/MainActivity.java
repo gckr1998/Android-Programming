1767f5cf330694fb0e31bcdcf8c4a1fa54dee389
Gurram Chandra Kishore Reddy <chandu@Gurrams-Air.bbrouter>
2022-10-15 12:14:58 +0530

    Added userRegistration Project

---
+++ First_App/app/src/main/java/com/example/userregistration/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.userregistration;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView firstNameView = findViewById(R.id.txtFirstNameView);
+    TextView secondNameView = findViewById(R.id.txtSecondNameView);
+    TextView emailView = findViewById(R.id.txtEmailView);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        firstNameView.setText("");
+        secondNameView.setText("");
+        emailView.setText("");
+    }
+    public void registerUser(View view){
+        EditText firstNameInput = findViewById(R.id.txtFirstNameInput);
+        EditText secondNameInput = findViewById(R.id.txtSecondNameInput);
+        EditText emailInput = findViewById(R.id.txtemailInput);
+
+        firstNameView.setText(firstNameInput.getText().toString());
+        secondNameView.setText(secondNameInput.getText().toString());
+        emailView.setText(emailInput.getText().toString());
+    }
+}
